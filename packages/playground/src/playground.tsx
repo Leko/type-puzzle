22204f730d7ef09d436871b93d96953b875a5efa
Leko <leko.noor@gmail.com>
2019-05-21 03:33:58 +0900

    fix restore compilerOptions

--- packages/playground/src/playground.tsx
+++ packages/playground/src/playground.tsx
@@ -105,7 +105,7 @@ export function Playground() {
   const handleRequestShare = useCallback(() => {
     const config = {
       code: code,
-      tsconfig: compilerOptions,
+      tsconfig: { compilerOptions },
       dependencies
     };
     new Share()
